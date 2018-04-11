1dc04740d81a47a3894af907dc7465d384161540
Andrew palmer <andy@showcasetech.net>
2018-04-11 15:41:16 -0500

    adding main routine

--- mls_loader.php
+++ mls_loader.php
@@ -1,4 +1,22 @@
 <?php
+//mls to be run is pulled from argv
+$mls = $argv[1];
+
+require('./config/$mls/config.php');
+
+date_default_timezone_set('America/New_York');
+
+require_once("vendor/autoload.php");
+
+$config = new \PHRETS\Configuration;
+$config->setLoginUrl('$url')
+        ->setUsername('$user')
+        ->setPassword('$pass')
+        ->setRetsVersion('$version');
+
+$rets = new \PHRETS\Session($config);
+
+$connect = $rets->Login();
 
 
 ?>
