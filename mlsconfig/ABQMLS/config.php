13ba8d12ef1d11fb48fd31c504d0fd6a418baf87
development@showcasetech.net <andy@showcasetech.net>
2018-04-16 14:32:59 -0500

    correct type

--- mlsconfig/ABQMLS/config.php
+++ mlsconfig/ABQMLS/config.php
@@ -10,7 +10,7 @@ $version = 'RETS/1.5';
 
 //listing search params
 $resource = 'Property';
-$classe = 'A';
+$class = 'A';
 $listingquery = '(LIST_15=1537DVHF564V,PWC_1DDI44AXS447,PWC_1DD44AXS9NC,PWC_1DDI44AXSFXJ)';
 
 //media search params
